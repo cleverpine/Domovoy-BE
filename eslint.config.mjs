545984b4a678019b477b4e9d09d432e3d3fcb347
Simona Georgieva <simona.georgieva@cleverpine.com>
2025-01-09 11:04:25 +0200

    additional eslint packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,34 @@
-import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import globals from 'globals';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
 export default [{
-    ignores: ["**/node_modules/", "**/dist/", "**/build/"],
-}, ...compat.extends("eslint:recommended", "airbnb-base"), {
-    languageOptions: {
-        globals: {
-            ...globals.node,
-        },
-
-        ecmaVersion: 12,
-        sourceType: "module",
+  ignores: ['**/node_modules/', '**/dist/', '**/build/'],
+}, ...compat.extends('eslint:recommended', 'airbnb-base'), {
+  languageOptions: {
+    globals: {
+      ...globals.node,
     },
 
-    rules: {
-        semi: ["error", "always"],
-        quotes: ["error", "single"],
-        indent: ["error", 2],
-        "arrow-body-style": "off",
-        "prefer-const": "warn",
-    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+
+  rules: {
+    semi: ['error', 'always'],
+    quotes: ['error', 'single'],
+    indent: ['error', 2],
+    'arrow-body-style': 'off',
+    'prefer-const': 'warn',
+  },
 }];
